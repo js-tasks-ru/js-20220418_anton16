bea319acb974f20dbd760504980c394b0de8533e
AKhorkin <horkin93@gmail.com>
2022-05-16 12:34:13 +0300

    Module 2 all tasks

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,21 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
 
+export function trimSymbols(string, size) {
+    if(typeof size === 'undefined') size = string.length;
+    let newString = [];
+    let symbol = '';
+    let index = 0;
+    for (var i = 0; i < string.length; i++) {
+        if(symbol !== string.charAt(i)){
+            symbol = string.charAt(i);
+            index = 0;
+        }
+        if(index < size){
+            newString.push(string.charAt(i));
+            index++;
+        }
+    }
+    return newString.join('');
 }
